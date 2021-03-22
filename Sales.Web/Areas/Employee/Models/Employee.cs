c97dc187982ba9689f8501ac1a92676325fb0bca
mahmoud_osama <46555700+MahmoudOsamaAli@users.noreply.github.com>
2021-03-22 15:35:48 +0200

    edit nav bar links

---
+++ Sales.Web/Areas/Employee/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sales.Web.Areas.Employee.Models
+{
+    public class Employee
+    {
+        public int ID { set; get; }
+        public string Name { set; get; }
+    }
+}
